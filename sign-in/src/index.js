7a7627de51c02314cb53a872b99dc4425d14662e
Eldar <sahaanonimus10@gmail.com>
2019-07-16 22:55:37 +0600

    chat

--- sign-in/src/index.js
+++ sign-in/src/index.js
@@ -6,13 +6,14 @@ import "./rbx.css";
 import { Input } from "rbx";
 import { Button } from "rbx";
 
-class Login_rbx extends Component {
+class Chat_rbx extends Component {
   constructor(props) {
     super(props);
     this.state = {
       valueOfMessage: "",
       time: new Date().toString(),
-      show: ""
+      show: "",
+      name: ""
     };
   }
   ChangeOfMessage = event => {
@@ -21,9 +22,14 @@ class Login_rbx extends Component {
   Send = () => {
     this.setState({ show: this.state.valueOfMessage });
   };
+  newChat = event => {
+    console.log(event.target.value);
+    this.setState({ name: event.target.value });
+  };
   render() {
     console.log(this.state.valueOfMessage);
     console.log(this.state.time);
+    console.log();
     return (
       <div className="messenger">
         <div className="chats">
@@ -31,26 +37,34 @@ class Login_rbx extends Component {
             <Input placeholder="search" />
             <hr />
             <div className="chat_user">
-              <p>Ilya</p>
+              <p onClick={this.newChat} value="Ilya">
+                Ilya
+              </p>
             </div>
             <hr />
             <div className="chat_user">
-              <p>Eldar</p>
+              <p onClick={this.newChat} value="Eldar">
+                Eldar
+              </p>
             </div>
             <hr />
             <div className="chat_user">
-              <p>Движ Париж</p>
+              <p onClick={this.newChat} value="Движ Париж">
+                Движ Париж
+              </p>
             </div>
             <hr />
             <div className="chat_user">
-              <p>Family</p>
+              <p onClick={this.newChat} value="Family">
+                Family
+              </p>
             </div>
             <hr />
           </div>
         </div>
         <div className="chat">
           <div className="name_of_chat">
-            <p>Ilya</p>
+            <p>{this.state.name}</p>
             <hr />
           </div>
           <p>{this.state.time}</p>
@@ -71,4 +85,45 @@ class Login_rbx extends Component {
     );
   }
 }
-ReactDOM.render(<Login_rbx />, document.getElementById("root"));
+ReactDOM.render(<Chat_rbx />, document.getElementById("root"));
+// train
+// class FlavorForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {value: 'coconut'};
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value});
+//   }
+
+//   handleSubmit(event) {
+//     alert('Your favorite flavor is: ' + this.state.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Pick your favorite flavor:
+//           <select value={this.state.value} onChange={this.handleChange}>
+//             <option value="grapefruit">Grapefruit</option>
+//             <option value="lime">Lime</option>
+//             <option value="coconut">Coconut</option>
+//             <option value="mango">Mango</option>
+//           </select>
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <FlavorForm />,
+//   document.getElementById('root')
+// );
