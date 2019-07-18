fb2cfa95528350e64315e84c376936bf2ee0ec45
Eldar <sahaanonimus10@gmail.com>
2019-07-18 23:27:53 +0600

    tasks

--- sign-in/src/index.js
+++ sign-in/src/index.js
@@ -13,6 +13,9 @@ class Chat_rbx extends Component {
       valueOfMessage: "",
       time: new Date().toString(),
       show: "",
+      show2: "",
+      show3: "",
+      show4: "",
       name: "",
       tab: ""
     };
@@ -20,88 +23,86 @@ class Chat_rbx extends Component {
   ChangeOfMessage = event => {
     this.setState({ valueOfMessage: event.target.value });
   };
-  Send = () => {
-    this.setState({ show: this.state.valueOfMessage });
-  };
+  // Send = () => {
+  //   this.setState({ show: this.state.valueOfMessage });
+  //   console.log(1);
+  // };
   newChat = event => {
-    console.log(event.target.value);
-    this.setState({ name: event.target.value });
-  };
-  tabs = event => {
     console.log(event.target.id);
-    this.setState({ tab: event.target.id });
+    this.setState({ name: event.target.id });
+    if (event.target.id === "Ilya") {
+      this.setState({ show: this.state.valueOfMessage });
+      this.setState({ tab: this.state.show });
+      console.log(1);
+    } else if (event.target.id === "Eldar") {
+      this.setState({ show2: this.state.valueOfMessage });
+      this.setState({ tab: this.state.show2 });
+      console.log(2);
+    } else if (event.target.id === "Family") {
+      this.setState({ show3: this.state.valueOfMessage });
+      this.setState({ tab: this.state.show3 });
+      console.log(4);
+    } else if (event.target.id === "Движ Париж") {
+      this.setState({ show4: this.state.valueOfMessage });
+      this.setState({ tab: this.state.show4 });
+      console.log(3);
+    }
   };
   render() {
     console.log(this.state.valueOfMessage);
     console.log(this.state.time);
-    console.log();
     return (
-      <div>
-        <button id={1} onClick={this.tabs}>
-          tab1
-        </button>
-        <button id={2} onClick={this.tabs}>
-          tab2
-        </button>
-        <button id={3} onClick={this.tabs}>
-          tab3
-        </button>
-        <button id={4} onClick={this.tabs}>
-          tab4
-        </button>
-        <p>{this.state.tab}</p>
+      <div className="messenger">
+        <div className="chats">
+          <div className="search">
+            <Input placeholder="search" />
+            <hr />
+            <div className="chat_user">
+              <p onClick={this.newChat} id="Ilya">
+                Ilya
+              </p>
+            </div>
+            <hr />
+            <div className="chat_user">
+              <p onClick={this.newChat} id="Eldar">
+                Eldar
+              </p>
+            </div>
+            <hr />
+            <div className="chat_user">
+              <p onClick={this.newChat} id="Движ Париж">
+                Движ Париж
+              </p>
+            </div>
+            <hr />
+            <div className="chat_user">
+              <p onClick={this.newChat} id="Family">
+                Family
+              </p>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div className="chat">
+          <div className="name_of_chat">
+            <p>{this.state.name}</p>
+            <hr />
+          </div>
+          <p>{this.state.time}</p>
+          <p>{this.state.tab}</p>
+          <div className="message">
+            <hr />
+            <input
+              placeholder="Write a message..."
+              value={this.state.valueOfMessage}
+              onChange={this.ChangeOfMessage}
+            />
+            <Button onClick={this.Send} className="send">
+              Send
+            </Button>
+          </div>
+        </div>
       </div>
-      // <div className="messenger">
-      //   <div className="chats">
-      //     <div className="search">
-      //       <Input placeholder="search" />
-      //       <hr />
-      //       <div className="chat_user">
-      //         <p onClick={this.newChat} value="Ilya">
-      //           Ilya
-      //         </p>
-      //       </div>
-      //       <hr />
-      //       <div className="chat_user">
-      //         <p onClick={this.newChat} value="Eldar">
-      //           Eldar
-      //         </p>
-      //       </div>
-      //       <hr />
-      //       <div className="chat_user">
-      //         <p onClick={this.newChat} value="Движ Париж">
-      //           Движ Париж
-      //         </p>
-      //       </div>
-      //       <hr />
-      //       <div className="chat_user">
-      //         <p onClick={this.newChat} value="Family">
-      //           Family
-      //         </p>
-      //       </div>
-      //       <hr />
-      //     </div>
-      //   </div>
-      //   <div className="chat">
-      //     <div className="name_of_chat">
-      //       <p>{this.state.name}</p>
-      //       <hr />
-      //     </div>
-      //     <p>{this.state.time}</p>
-      //     <p>{this.state.show}</p>
-      //     <div className="message">
-      //       <hr />
-      //       <input
-      //         placeholder="Write a message..."
-      //         value={this.state.valueOfMessage}
-      //         onChange={this.ChangeOfMessage}
-      //       />
-      //       <Button onClick={this.Send} className="send">
-      //         Send
-      //       </Button>
-      //     </div>
-      // </div>
-      //</div>
     );
   }
 }
