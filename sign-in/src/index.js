8243b36db2c068edfc5f7f64369a32a37f62cbb2
Eldar <sahaanonimus10@gmail.com>
2019-07-19 23:30:59 +0600

    design-of-chat

--- sign-in/src/index.js
+++ sign-in/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // import Sign_in from "./components/Sign_in";
-// import "./style.css";
+//import "./style.css";
 import "./rbx.css";
 import { Input } from "rbx";
 import { Button } from "rbx";
@@ -9,13 +9,15 @@ import { Button } from "rbx";
 class Chat_rbx extends Component {
   constructor(props) {
     super(props);
+    // у каждого чата есть свой массив в котором хранятся сообщения
     this.state = {
       valueOfMessage: "",
       time: new Date().toString(),
-      show: "",
-      show2: "",
-      show3: "",
-      show4: "",
+      show: [],
+      show2: [],
+      show3: [],
+      show4: [],
+      currentChat: "show",
       name: "",
       tab: ""
     };
@@ -23,28 +25,35 @@ class Chat_rbx extends Component {
   ChangeOfMessage = event => {
     this.setState({ valueOfMessage: event.target.value });
   };
-  // Send = () => {
-  //   this.setState({ show: this.state.valueOfMessage });
-  //   console.log(1);
-  // };
+
+  // вот здесь самая главная часть нашей логики
+  // мы отправляем сообщение в ОПРЕДЕЛЕННУЮ часть от state
+  // и стираем сообщение после отправки
+  Send = () => {
+    let newState = this.state[this.state.currentChat];
+    newState.push(this.state.valueOfMessage);
+    this.setState({ show: newState, valueOfMessage: "" });
+    console.log(1);
+  };
+
   newChat = event => {
     console.log(event.target.id);
-    this.setState({ name: event.target.id });
+    // когда переключаем чат то обнуляем строку сообщения
+    // и даем ему емя
+    this.setState({ valueOfMessage: "", name: event.target.id });
+
+    // здесь мы просто переключаем окно
     if (event.target.id === "Ilya") {
-      this.setState({ show: this.state.valueOfMessage });
-      this.setState({ tab: this.state.show });
+      this.setState({ currentChat: "show" });
       console.log(1);
     } else if (event.target.id === "Eldar") {
-      this.setState({ show2: this.state.valueOfMessage });
-      this.setState({ tab: this.state.show2 });
+      this.setState({ currentChat: "show2" });
       console.log(2);
     } else if (event.target.id === "Family") {
-      this.setState({ show3: this.state.valueOfMessage });
-      this.setState({ tab: this.state.show3 });
+      this.setState({ currentChat: "show3" });
       console.log(4);
     } else if (event.target.id === "Движ Париж") {
-      this.setState({ show4: this.state.valueOfMessage });
-      this.setState({ tab: this.state.show4 });
+      this.setState({ currentChat: "show4" });
       console.log(3);
     }
   };
@@ -58,27 +67,43 @@ class Chat_rbx extends Component {
             <Input placeholder="search" />
             <hr />
             <div className="chat_user">
-              <p onClick={this.newChat} id="Ilya">
+              <div
+                onClick={this.newChat}
+                id="Ilya"
+                style={{ cursor: "pointer" }}
+              >
                 Ilya
-              </p>
+              </div>
             </div>
             <hr />
             <div className="chat_user">
-              <p onClick={this.newChat} id="Eldar">
+              <div
+                onClick={this.newChat}
+                id="Eldar"
+                style={{ cursor: "pointer" }}
+              >
                 Eldar
-              </p>
+              </div>
             </div>
             <hr />
             <div className="chat_user">
-              <p onClick={this.newChat} id="Движ Париж">
+              <div
+                onClick={this.newChat}
+                id="Движ Париж"
+                style={{ cursor: "pointer" }}
+              >
                 Движ Париж
-              </p>
+              </div>
             </div>
             <hr />
             <div className="chat_user">
-              <p onClick={this.newChat} id="Family">
+              <div
+                onClick={this.newChat}
+                id="Family"
+                style={{ cursor: "pointer" }}
+              >
                 Family
-              </p>
+              </div>
             </div>
             <hr />
           </div>
@@ -89,7 +114,12 @@ class Chat_rbx extends Component {
             <hr />
           </div>
           <p>{this.state.time}</p>
-          <p>{this.state.tab}</p>
+          {/**
+
+          */}
+          {this.state[this.state.currentChat].map(message => (
+            <p key={message}>{message}</p>
+          ))}
           <div className="message">
             <hr />
             <input
